7092d30d5eae926ad9c56cf2568dce6b0169abba
rares.amza <rares.amza@ROMOB41159>
2024-03-07 15:36:02 +0200

    added greeting

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! From Alex");
+
+greetingsMessage("Hello!!!!");
+
+static string greetingsMessage(string greetingsMessage) => greetingsMessage;
