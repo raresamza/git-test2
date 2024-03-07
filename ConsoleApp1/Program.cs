87174656382aa223c84ea988d1de5e3ad9fc568a
rares.amza <rares.amza@ROMOB41159>
2024-03-07 15:46:01 +0200

    "added more greetings

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 greetingsMessage("Hello!!!!");
+greetingsMessage("Hello!!!!");
+greetingsMessage("Hello!!!!");
 
 static string greetingsMessage(string greetingsMessage) => greetingsMessage;
